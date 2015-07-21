0cdad9cd8bfd2633fde741cc2dec62dcc75ae07b
Allen Tsai <allentsai93@gmail.com>
2015-07-20 22:41:03 -0700

    finished product v1

--- chequeplease/ViewController.swift
+++ chequeplease/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var peopleSlider: UISlider!
+    
+    @IBOutlet var peopleLabel: UILabel!
+    
+    @IBOutlet var chequeLabel: UILabel!
+
+    @IBOutlet var chequeSlider: UISlider!
+    
+    @IBOutlet var tipLabel: UILabel!
+    
+    @IBOutlet var tipSlider: UISlider!
+    
+    @IBOutlet var result: UILabel!
+    
+    @IBAction func peopleSlided(sender: AnyObject) {
+        var peopleValue = Int(peopleSlider.value)
+        peopleLabel.text = "\(peopleValue)"
+        
+        if peopleValue == 0 {
+            peopleLabel.text = "Number of People"
+        }
+    }
+
+    @IBAction func chequeSlided(sender: AnyObject) {
+        var chequeValue = Int(chequeSlider.value)
+        chequeLabel.text = "\(chequeValue)"
+        
+        if chequeValue == 0 {
+            chequeLabel.text = "Cost of Cheque"
+        }
+    }
+    @IBAction func tipSlided(sender: AnyObject) {
+        var tipValue = Int(tipSlider.value)
+        tipLabel.text = "\(tipValue)%"
+        
+        if tipValue == 0 {
+            tipLabel.text = "Tip %"
+        }
+    }
+    
+    @IBAction func calculateTotal(sender: AnyObject) {
+        var resultWoTip = Int(chequeSlider.value) / Int(peopleSlider.value);
+        
+        println(resultWoTip)
+        
+        var resultWTip = Double(resultWoTip) * (1 + (Double(tipSlider.value) / 100))
+        
+        var roundedResult = round(100*resultWTip)/100
+        
+        result.text = "\(roundedResult)"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

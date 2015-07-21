cf7ef838f86852d1f471cea74bdec5058a423811
Allen Tsai <allentsai93@gmail.com>
2015-07-20 20:51:33 -0700

    Initial Commit

---
+++ chequeplease/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chequeplease
+//
+//  Created by Allen Tsai on 2015-07-20.
+//  Copyright (c) 2015 Allen Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
